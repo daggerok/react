e8fc3080789de1826b49c28a43ad371d40915ebe
Maksim Kostromin <daggerok@gmail.com>
2016-11-07 03:49:28 +0200

    add default props

--- src/components/Hi.jsx
+++ src/components/Hi.jsx
@@ -1,3 +1,15 @@
+/*
 const Hi = ({ name }) => (
   <div>hi, {name ? name : 'guest'}</div>
 );
+*/
+
+class Hi extends React.Component {
+  static defaultProps = {
+    name: 'guest',
+  };
+
+  render() {
+    return <div>hi, {this.props.name}</div>;
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,4 @@
-/**
- * @license
- * Copyright daggerok. All rights reserved.
- *
- * Use of this source code is governed by a ISC-style license
- * that can be found in the LICENSE file. at https://github.com/daggerok/angular2/LICENSE
- */
-/* eslint no-unused-vars: "off" */
-
 ReactDOM.render(
-  <Hi name='Max'/>,
+  <Hi/>,
   document.getElementById('app')
 );
