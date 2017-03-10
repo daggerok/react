1f2292f24c07eff4e1d03c4648d9e914fb1e9bc8
Maksim Kostromin <daggerok@gmail.com>
2017-03-10 04:05:46 +0200

    update npm-scripts

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -371,7 +371,7 @@ module.exports = env => ({
 
     new CompressionWebpackPlugin({
       asset: '[path].gz?[query]', // default: [path].gz[query]
-      algorithm: 'gzip', // zlib, zopfli, function(buf, callback)
+      // algorithm: 'gzip', // zlib, zopfli, function(buf, callback)
       // test: /\.(js|css|html)$/i, // default: every assets
       // threshold: 10240, // default: 0
       // minRatio: 0.8 // default: Only assets that compress better that ratio: 0.8
