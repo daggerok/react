f460eb8ce3d0410b1d78cd507c8f69d0ec99a2fe
Maksim Kostromin <daggerok@gmail.com>
2017-03-10 03:54:30 +0200

    update webpack

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -284,9 +284,14 @@ module.exports = env => ({
     !isProd(env) ? new HotModuleReplacementPlugin(): undefined,
 
     isProd(env) ? new UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true
+      },
       compress: {
         warnings: false,
-        // screw_ie8: true,
+        screw_ie8: true,
         // conditionals: true,
         // unused: true,
         // comparisons: true,
